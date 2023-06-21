59fd7274b64c418b4105884363d28110ee84a97c
shohjahon <komiljonovshohjahon@gmail.com>
2023-06-21 11:04:47 +0900

    Revert "update"
    This reverts commit c7ffc3cce3050117be8467945f560cfdeff98bb8.

---
+++ mason_creatego_cli/lib/generator.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'package:mason/mason.dart';
+
+bool isLinux = false;
+
+const String _linuxPath =
+    "/home/creatego-server/Documents/creatego/creatego_generator_server/mason_creatego_cli/gen/bricks/widget_template";
+const String _windowsPath =
+    "C:/Users/komil/Documents/GitHub/nocode/creatego_generator_server/mason_creatego_cli/gen/bricks/widget_template";
+
+class Generator {
+  static Future<String> generateWidget(Map<String, dynamic> json) async {
+    final brick = Brick.path(isLinux ? _linuxPath : _windowsPath);
+    final generator = await MasonGenerator.fromBrick(brick);
+    final target = DirectoryGeneratorTarget(Directory.current);
+    final res = await generator.generate(
+      target,
+      vars: json,
+      fileConflictResolution: FileConflictResolution.overwrite,
+      logger: Logger(theme: LogTheme()),
+    );
+    return res.first.path;
+  }
+}
